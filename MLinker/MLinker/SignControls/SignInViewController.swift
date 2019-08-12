6e2b277eee9382d2ea57f4725219ef8314bb5a38
John DongHyun Kim <zapping0320@gmail.com>
2019-08-13 06:52:24 +0900

    add viewcontrollers at main tab controllers

--- MLinker/MLinker/SignControls/SignInViewController.swift
+++ MLinker/MLinker/SignControls/SignInViewController.swift
@@ -1,8 +1,8 @@
 //
 //  SignInViewController.swift
-//  MLinker
+//  SignIn
 //
-//  Created by 김동현 on 13/08/2019.
+//  Created by 김동현 on 08/07/2019.
 //  Copyright © 2019 John Kim. All rights reserved.
 //
 
@@ -10,21 +10,86 @@ import UIKit
 
 class SignInViewController: UIViewController {
 
+    let loginURLString = "http://localhost:3000/loginUsers"
+    
+    @IBOutlet weak var userNameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        SignInButton.layer.cornerRadius = SignInButton.bounds.size.height / 2
+        SignInButton.layer.borderWidth = 1
+        SignInButton.layer.borderColor = UIColor.blue.cgColor
+        
+        self.navigationController?.setNavigationBarHidden(true, animated: false)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func dismissVC(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
     }
-    */
-
+    
+    @IBOutlet weak var SignInButton: UIButton!
+    @IBAction func moveToSignup(_ sender: Any) {
+        let signupVC = UIStoryboard(name: "SignUp", bundle: nil).instantiateViewController(withIdentifier: "SignUpViewController")
+        
+        self.navigationController?.pushViewController(signupVC, animated: true)
+    }
+    
+    @IBAction func signinAPICall(_ sender: Any) {
+//        //params
+//        //get post
+//        //URL Session
+//        let param = [
+//            "userName" : userNameTextField.text ?? "",
+//            "password" : passwordTextField.text ?? ""
+//        ]
+//        
+//        //query string
+//     
+//        //http://localhost:3000/loginUsers
+//        
+//         //let loginURL = URL(string: loginURLString + "?" + param.queryString)
+//        
+//        //URL Components
+//        var urlComponents = URLComponents(string: loginURLString)
+//        
+//        urlComponents?.query = param.queryString
+//        
+//        guard let hasURL = urlComponents?.url else {
+//            return
+//        }
+//        
+//        URLSession.shared.dataTask(with: hasURL) { (data, response, error) in
+//            
+//            guard let data = data else {
+//                return
+//            }
+//            
+//            let decoder = JSONDecoder()
+//            do {
+//                let users = try decoder.decode([LoginUser].self, from: data)
+//                if let hasUserInfo = users.first {
+//                    User.shared.info = hasUserInfo
+//                    
+//                    self.dismiss(animated: true, completion: nil)
+//                    
+//                    NotificationCenter.default.post(name: NSNotification.Name.init(rawValue: "UserInfoLoad"), object: nil)
+//                    
+//                }else {
+//                    DispatchQueue.main.async {
+//                        let alert = UIAlertController.init(title: "No User Info", message: nil, preferredStyle: .alert)
+//                        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+//                        self.present(alert, animated: true, completion: nil)
+//                    }
+//                }
+//            }catch {
+//                //error
+//                print("error = \(error)")
+//            }
+//           
+//        }.resume()
+//        
+        
+    }
 }
+
--- MLinker/MLinker/SignControls/SignUpViewController.swift
+++ MLinker/MLinker/SignControls/SignUpViewController.swift
@@ -1,8 +1,8 @@
 //
 //  SignUpViewController.swift
-//  MLinker
+//  SignIn
 //
-//  Created by 김동현 on 13/08/2019.
+//  Created by 김동현 on 09/07/2019.
 //  Copyright © 2019 John Kim. All rights reserved.
 //
 
@@ -10,21 +10,57 @@ import UIKit
 
 class SignUpViewController: UIViewController {
 
+    @IBOutlet weak var userName: UITextField!
+    @IBOutlet weak var password: UITextField!
+    @IBOutlet weak var email: UITextField!
+    @IBOutlet weak var SignUpButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        SignUpButton.layer.cornerRadius = SignUpButton.bounds.size.height / 2
+        SignUpButton.layer.borderWidth = 1
+        SignUpButton.layer.borderColor = UIColor.blue.cgColor
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func popVC(_ sender: Any) {
+        self.navigationController?.popViewController(animated: true)
     }
-    */
-
+    @IBAction func signupApiCAll(_ sender: Any) {
+//        let param = [
+//            "userName"  : userName.text ?? "",
+//            "password"  : password.text ?? "",
+//            "email"     : email.text ?? ""
+//        ]
+//
+//        //HTTP Method -> POST
+//        if let url = URL(string: "http://localhost:3000/loginUsers") {
+//            var request = URLRequest.init(url: url)
+//
+//            request.httpMethod = "POST"
+//            request.httpBody = param.queryString.data(using: .utf8)
+//
+//            URLSession.shared.dataTask(with: request) { (data, response, error) in
+//                guard let data = data else {
+//                    return
+//                }
+//
+//                let decoder = JSONDecoder()
+//                do {
+//                    let user = try decoder.decode(LoginUser.self, from: data)
+//
+//                    User.shared.info = user
+//
+//                    self.dismiss(animated: true, completion: nil)
+//
+//                    NotificationCenter.default.post(name: NSNotification.Name.init(rawValue: "UserInfoLoad"), object: nil)
+//
+//                }catch {
+//                    //error
+//                    print("error = \(error)")
+//                }
+//
+//
+//            }.resume()
+//        }
+//
+    }
 }
