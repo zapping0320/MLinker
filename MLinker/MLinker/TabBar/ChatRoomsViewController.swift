c9679f975fe643e586af23b98f37f38d8b7b6811
John DongHyun Kim <zapping0320@gmail.com>
2019-08-13 06:29:13 +0900

    add eacth tap view Controllers before implementing each functions

---
+++ MLinker/MLinker/TabBar/ChatRoomsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ChatRoomsViewController.swift
+//  MLinker
+//
+//  Created by 김동현 on 13/08/2019.
+//  Copyright © 2019 John Kim. All rights reserved.
+//
+
+import UIKit
+
+class ChatRoomsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
