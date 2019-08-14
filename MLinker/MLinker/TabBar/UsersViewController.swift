ab27a3a02755f35bdc06f8ead960132a633dda12
John DongHyun Kim <zapping0320@gmail.com>
2019-08-15 07:03:16 +0900

    implementing signin navigation VC at starting app

--- MLinker/MLinker/TabBar/UsersViewController.swift
+++ MLinker/MLinker/TabBar/UsersViewController.swift
@@ -12,19 +12,39 @@ class UsersViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
+         //self.displayWelcome()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewDidAppear(_ animated: Bool) {
+        self.displayWelcome()
     }
-    */
+    
+    func displayWelcome() {
+        //        let color = remoteConfig["splash_background"].stringValue
+        //        let caps = remoteConfig["splash_message_caps"].boolValue
+        //        let message = remoteConfig["splash_message"].stringValue
+        //
+        //        if(caps){
+        //            let alert = UIAlertController(title: "Notice", message: message, preferredStyle: .alert)
+        //            alert.addAction(UIAlertAction(title:"OK", style: .default, handler: { (action) in
+        //                exit(0)
+        //            }))
+        //
+        //            self.present(alert, animated: true, completion: nil)
+        //        }else {
+        if(true) {//not signed in
+            let signInVC = UIStoryboard(name: "SigninStoryboard", bundle: nil).instantiateViewController(withIdentifier: "naviSignin")
+            self.present(signInVC, animated: true, completion: nil)
+        //let signupVC = UIStoryboard(name: "SignUp", bundle: nil).instantiateViewController(withIdentifier: "SignUpViewController")
+            
+        //self.present(signupVC, animated: true, completion: nil)
+        }else {
+            //signed in
+        }
+        //        }
+        //        self.view.backgroundColor = UIColor(hex: color!)
+    }
+
 
 }
