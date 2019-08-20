46d481bdbf12140b7c8ae99de72dae8b1766965e
John DongHyun Kim <zapping0320@gmail.com>
2019-08-21 07:28:58 +0900

    implementing signin at signIn VC
    not yet test
    will test after implementing sign up

--- MLinker/MLinker/SignControls/SignInViewController.swift
+++ MLinker/MLinker/SignControls/SignInViewController.swift
@@ -7,11 +7,16 @@
 //
 
 import UIKit
+import Firebase
 
 class SignInViewController: UIViewController {
     
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var SignInButton: UIButton!
+    
+    let firebaseAuth = Auth.auth()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -20,74 +25,50 @@ class SignInViewController: UIViewController {
         SignInButton.layer.borderColor = UIColor.blue.cgColor
         
         self.navigationController?.setNavigationBarHidden(true, animated: false)
+        
+        signeOut()
+        
+        firebaseAuth.addStateDidChangeListener {
+            (auth, user)
+            in
+            if(user != nil) {
+               self.dismiss(animated: true, completion: nil)
+            }
+        }
+        
     }
     
     @IBAction func dismissVC(_ sender: Any) {
         self.dismiss(animated: true, completion: nil)
     }
-    
-    @IBOutlet weak var SignInButton: UIButton!
+  
     @IBAction func moveToSignup(_ sender: Any) {
         let signupVC = UIStoryboard(name: "SignUp", bundle: nil).instantiateViewController(withIdentifier: "SignUpViewController")
         
         self.navigationController?.pushViewController(signupVC, animated: true)
     }
     
-    @IBAction func signinAPICall(_ sender: Any) {
-//        //params
-//        //get post
-//        //URL Session
-//        let param = [
-//            "userName" : userNameTextField.text ?? "",
-//            "password" : passwordTextField.text ?? ""
-//        ]
-//        
-//        //query string
-//     
-//        //http://localhost:3000/loginUsers
-//        
-//         //let loginURL = URL(string: loginURLString + "?" + param.queryString)
-//        
-//        //URL Components
-//        var urlComponents = URLComponents(string: loginURLString)
-//        
-//        urlComponents?.query = param.queryString
-//        
-//        guard let hasURL = urlComponents?.url else {
-//            return
-//        }
-//        
-//        URLSession.shared.dataTask(with: hasURL) { (data, response, error) in
-//            
-//            guard let data = data else {
-//                return
-//            }
-//            
-//            let decoder = JSONDecoder()
-//            do {
-//                let users = try decoder.decode([LoginUser].self, from: data)
-//                if let hasUserInfo = users.first {
-//                    User.shared.info = hasUserInfo
-//                    
-//                    self.dismiss(animated: true, completion: nil)
-//                    
-//                    NotificationCenter.default.post(name: NSNotification.Name.init(rawValue: "UserInfoLoad"), object: nil)
-//                    
-//                }else {
-//                    DispatchQueue.main.async {
-//                        let alert = UIAlertController.init(title: "No User Info", message: nil, preferredStyle: .alert)
-//                        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
-//                        self.present(alert, animated: true, completion: nil)
-//                    }
-//                }
-//            }catch {
-//                //error
-//                print("error = \(error)")
-//            }
-//           
-//        }.resume()
-//        
-        
+    @IBAction func signIn(_ sender: Any) {
+        Auth.auth().signIn(withEmail: self.emailTextField.text!, password: self.passwordTextField.text!) {
+            (user, error) in
+            
+            if( error != nil ) {
+                let alert = UIAlertController(title: "error", message: error.debugDescription, preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+                self.present(alert, animated: true, completion: nil)
+            }
+        }
+    }
+    
+    //will beremoved after implementing
+    func signeOut() {
+        do {
+            try firebaseAuth.signOut()
+        } catch let signOutError as NSError {
+            print ("Error signing out: %@", signOutError)
+        }
+        self.emailTextField.text? = "bmwe3@hanmail.net"
+        self.passwordTextField.text? = "@1234asdf"
     }
 }
 
