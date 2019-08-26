3f64e6987a0c5a6030eeccadf567544ece71e63b
John DongHyun Kim <zapping0320@gmail.com>
2019-08-27 07:03:16 +0900

    add UserTableViewCell for UsersVC

---
+++ MLinker/MLinker/CommonControls/UserTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  UserTableViewCell.swift
+//  MLinker
+//
+//  Created by 김동현 on 27/08/2019.
+//  Copyright © 2019 John Kim. All rights reserved.
+//
+
+import UIKit
+
+class UserTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var commentLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
