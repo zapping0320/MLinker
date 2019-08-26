f962ef4dac50e43bb8330dbedcd2873ba3821d35
John DongHyun Kim <zapping0320@gmail.com>
2019-08-27 07:22:30 +0900

    add userstableview at UsersVC with sample data
    todo
    apply server's data instead of sample data

--- MLinker/MLinker/TabBar/UsersViewController.swift
+++ MLinker/MLinker/TabBar/UsersViewController.swift
@@ -9,17 +9,52 @@
 import UIKit
 import Firebase
 
-class UsersViewController: UIViewController {
-
+class UsersViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
+    @IBOutlet weak var usersTableView: UITableView!
+    
+    var userArray: [UserModel] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+         self.usersTableView.register(UINib(nibName: "UserTableViewCell", bundle: nil), forCellReuseIdentifier: "UserCell")
     }
     
     override func viewDidAppear(_ animated: Bool) {
-           
+        let userModel = UserModel()
+        userModel.uid = "123"
+        userModel.name = "kkk"
+        userModel.comment = "comment"
+        self.userArray.append(userModel)
+        
+        let userModel2 = UserModel()
+        userModel2.uid = "123"
+        userModel2.name = "gggg"
+        userModel2.comment = "commentary"
+        self.userArray.append(userModel2)
+        
+        DispatchQueue.main.async {
+            self.usersTableView.reloadData()
+        }
         
     }
+    
+}
 
+
+extension UsersViewController {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.userArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+         let cell = tableView.dequeueReusableCell(withIdentifier: "UserCell", for: indexPath) as! UserTableViewCell
+        let currentUser = self.userArray[indexPath.row]
+        
+        cell.nameLabel?.text = currentUser.name
+        cell.commentLabel?.text = currentUser.comment
+        
+        return cell
+    }
 }
