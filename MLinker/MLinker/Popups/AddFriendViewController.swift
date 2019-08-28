cf919ac5bfb3d0fe6770c3287162e0ea6ee513fe
John DongHyun Kim <zapping0320@gmail.com>
2019-08-29 07:06:49 +0900

    set layout addFriend VC

--- MLinker/MLinker/Popups/AddFriendViewController.swift
+++ MLinker/MLinker/Popups/AddFriendViewController.swift
@@ -10,21 +10,40 @@ import UIKit
 
 class AddFriendViewController: UIViewController {
 
+    @IBOutlet weak var applyButton: UIButton!
+    @IBOutlet weak var friendEmailTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        applyButton.layer.cornerRadius = applyButton.bounds.size.height / 2
+        applyButton.layer.borderWidth = 1
+        applyButton.layer.borderColor = UIColor.blue.cgColor
+        
+        setApplyButtonEnabled(value: false)
+        
+        self.navigationController?.setNavigationBarHidden(true, animated: false)
 
-        // Do any additional setup after loading the view.
+    }
+    
+    func setApplyButtonEnabled(value : Bool) {
+        if(value){
+            applyButton.isEnabled = true
+            applyButton.layer.borderColor = UIColor.blue.cgColor
+            applyButton.setTitleColor(.white, for: .normal)
+        }
+        else
+        {
+            applyButton.isEnabled = false
+            applyButton.layer.borderColor = UIColor.gray.cgColor
+            applyButton.setTitleColor(.gray, for: .normal)
+        }
     }
 
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func dismissVC(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
     }
-    */
-
+    
+    @IBAction func applyFriendShip(_ sender: Any) {
+        
+    }
 }
