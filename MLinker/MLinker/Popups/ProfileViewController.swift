db6b0da6d5112b239a9f09c203c00ff72787e499
John DongHyun Kim <zapping0320@gmail.com>
2019-09-10 06:51:32 +0900

    set profile VC layout

--- MLinker/MLinker/Popups/ProfileViewController.swift
+++ MLinker/MLinker/Popups/ProfileViewController.swift
@@ -10,20 +10,50 @@ import UIKit
 
 class ProfileViewController: UIViewController {
 
+    @IBOutlet weak var commentLabel: UILabel!
+    @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var mainButton: UIButton!
+    @IBOutlet weak var subButton: UIButton!
+    
+    
     public var selectedUserModel: UserModel = UserModel()
     public var selectedFriendshipModel : FriendshipModel?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        mainButton.layer.cornerRadius = mainButton.bounds.size.height / 2
+        mainButton.layer.borderWidth = 1
+        mainButton.layer.borderColor = UIColor.blue.cgColor
+        
+        subButton.layer.cornerRadius = subButton.bounds.size.height / 2
+        subButton.layer.borderWidth = 1
+        subButton.layer.borderColor = UIColor.blue.cgColor
+        
         if(selectedFriendshipModel != nil)
         {
-            print(self.selectedFriendshipModel?.friendEmail)
+            if(selectedFriendshipModel?.status == FriendStatus.Requesting){
+                self.mainButton.setTitle("cancel Request", for: .normal)
+                self.subButton.isHidden = true
+            }else if(selectedFriendshipModel?.status == FriendStatus.Requesting){
+                self.mainButton.setTitle("accept Request", for: .normal)
+                self.subButton.setTitle("reject Request", for: .normal)
+            }
+        }
+        else
+        {
+            
         }
         
     }
     
     @IBAction func closeVC(_ sender: Any) {
         self.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func mainButtonAction(_ sender: Any) {
+    }
+    
+    @IBAction func subButtonAction(_ sender: Any) {
     }
 }
