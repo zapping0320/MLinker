a5812d598a105b9997dd294f0255b34999a383ca
John DongHyun Kim <zapping0320@gmail.com>
2019-09-18 06:59:09 +0900

    start layout of chats VC

--- MLinker/MLinker/TabBar/ChatRoomsViewController.swift
+++ MLinker/MLinker/TabBar/ChatRoomsViewController.swift
@@ -8,23 +8,28 @@
 
 import UIKit
 
-class ChatRoomsViewController: UIViewController {
-
+class ChatRoomsViewController: UIViewController,UITableViewDelegate, UITableViewDataSource {
+    @IBOutlet weak var chatRoomTableView: UITableView!
+    var chatRooms: [ChatModel]! = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+       self.chatRoomTableView.register(UINib(nibName: "ChatRoomTableViewCell", bundle: nil), forCellReuseIdentifier: "ChatRoomCell")
+        self.chatRoomTableView.delegate = self
+        self.chatRoomTableView.dataSource = self
+    }
+
+}
+
+extension ChatRoomsViewController {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return chatRooms.count
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ChatRoomCell", for: indexPath) as! ChatRoomTableViewCell
+        
+        return cell
     }
-    */
-
 }
