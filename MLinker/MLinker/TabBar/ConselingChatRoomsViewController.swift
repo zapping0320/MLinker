cb2f2a93d255cb556fd1af5ec2fff7ad32aaf74c
John DongHyun Kim <zapping0320@gmail.com>
2019-10-07 07:28:38 +0900

    implement Conseling Layout

--- MLinker/MLinker/TabBar/ConselingChatRoomsViewController.swift
+++ MLinker/MLinker/TabBar/ConselingChatRoomsViewController.swift
@@ -7,24 +7,75 @@
 //
 
 import UIKit
+import Firebase
+import Kingfisher
 
-class ConselingChatRoomsViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+class ConselingChatRoomsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    @IBOutlet weak var chatRoomTableView: UITableView!{
+        didSet {
+            self.chatRoomTableView.delegate = self
+            self.chatRoomTableView.dataSource = self
+        }
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    var currnetUserUid: String!
+    var chatRooms: [ChatModel]! = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.chatRoomTableView.register(UINib(nibName: "ChatRoomTableViewCell", bundle: nil), forCellReuseIdentifier: "ChatRoomCell")
+        
+        self.currnetUserUid = Auth.auth().currentUser?.uid
+        
     }
-    */
+    
+    override func viewDidAppear(_ animated: Bool) {
+        self.getChatRoomsList()
+    }
+    
+    func getChatRoomsList() {
+        Database.database().reference().child("chatRooms").observeSingleEvent(of: DataEventType.value) {
+            (datasnapShot) in
+            self.chatRooms.removeAll()
+            for item in datasnapShot.children.allObjects as! [DataSnapshot] {
+                if let chatRoomdic = item.value as? [String:AnyObject] {
+                    let chatModel = ChatModel(JSON: chatRoomdic)
+                    chatModel?.uid = item.key
+                    if((chatModel?.chatUserIdDic.keys.contains(self.currnetUserUid!)) != true || chatModel?.isIncludeAdminAccount == false)
+                    {
+                        continue
+                    }
+                    self.chatRooms.append(chatModel!)
+                }
+            }
+            DispatchQueue.main.async {
+                self.chatRoomTableView.reloadData()
+            }
+        }
+    }
 
+}
+
+extension ConselingChatRoomsViewController {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return chatRooms.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ChatRoomCell", for: indexPath) as! ChatRoomTableViewCell
+        
+        let chatRoom = self.chatRooms[indexPath.row]
+        
+        cell.nameLabel.text = chatRoom.name
+        
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let chatVC = UIStoryboard(name: "ChatView", bundle: nil).instantiateViewController(withIdentifier: "IdChatView") as! ChatViewController
+        chatVC.selectedChatModel = self.chatRooms[indexPath.row]
+        self.navigationController?.pushViewController(chatVC, animated: true)
+    }
 }
