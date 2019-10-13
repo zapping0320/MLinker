ee7cfc9a0db5f1c8138056415a8991bb0ebe539f
John DongHyun Kim <zapping0320@gmail.com>
2019-10-14 06:58:31 +0900

    implement cancel function at AddChatRoomVC

--- MLinker/MLinker/Popups/AddChatRoomViewController.swift
+++ MLinker/MLinker/Popups/AddChatRoomViewController.swift
@@ -13,18 +13,17 @@ class AddChatRoomViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+       
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func cancelAddChatRoom(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
     }
-    */
+    
+    @IBAction func doneAddChatRoom(_ sender: Any) {
+        
+        
+    }
+    
 
 }
