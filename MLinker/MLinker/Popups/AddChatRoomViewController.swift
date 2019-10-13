69761dbbdb098ee318dc3c03e494a017f2e02771
John DongHyun Kim <zapping0320@gmail.com>
2019-10-14 07:14:29 +0900

    add tableview at AddChatRoomVC

--- MLinker/MLinker/Popups/AddChatRoomViewController.swift
+++ MLinker/MLinker/Popups/AddChatRoomViewController.swift
@@ -7,11 +7,25 @@
 //
 
 import UIKit
+import Firebase
+import Kingfisher
 
-class AddChatRoomViewController: UIViewController {
+class AddChatRoomViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var usersTableView: UITableView!{
+        didSet {
+            self.usersTableView.delegate = self
+            self.usersTableView.dataSource = self
+        }
+    }
+    
+    
+    fileprivate var usersArray: [Int:[UserModel]] = [Int:[UserModel]]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.usersTableView.register(UINib(nibName: "UserTableViewCell", bundle: nil), forCellReuseIdentifier: "UserCell")
 
        
     }
@@ -24,6 +38,67 @@ class AddChatRoomViewController: UIViewController {
         
         
     }
+}
+
+extension AddChatRoomViewController {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
     
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        guard let dataList = usersArray[section] else {
+            return 0
+        }
+        return dataList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+         let cell = tableView.dequeueReusableCell(withIdentifier: "UserCell", for: indexPath) as! UserTableViewCell
+        let currentUser = self.usersArray[indexPath.section]![indexPath.row] as UserModel
+       
+        cell.setAdminAccount(value: currentUser.isAdminAccount)
+        cell.nameLabel?.text = currentUser.name
+        cell.commentLabel?.text = currentUser.comment
+        
+        if let profileImageString = currentUser.profileURL {
+            let profileImageURL = URL(string: profileImageString)
+            let processor = DownsamplingImageProcessor(size: CGSize(width: 80, height: 80))
+                >> RoundCornerImageProcessor(cornerRadius: 40)
+            cell.profileImageView?.kf.indicatorType = .activity
+            cell.profileImageView?.kf.setImage(
+                with: profileImageURL,
+                placeholder: UIImage(named: "defaultPhoto"),
+                options: [
+                    .processor(processor),
+                    .scaleFactor(UIScreen.main.scale),
+                    .transition(.fade(1)),
+                    .cacheOriginalImage
+                ])
+            {
+                result in
+                switch result {
+                case .success(let value):
+                    print("Task done for: \(value.source.url?.absoluteString ?? "")")
+                case .failure(let error):
+                    print("Job failed: \(error.localizedDescription)")
+                }
+            }
+            
+        }
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("did select \(indexPath)")
+//        let profileVC = UIStoryboard(name: "Profile", bundle: nil).instantiateViewController(withIdentifier: "profileNavi") as! ProfileViewController
+//
+//        profileVC.selectedUserModel = self.usersArray[indexPath.section]![indexPath.row] as UserModel
+//        if(indexPath.section == 1) {
+//            profileVC.selectedFriendshipModel = self.processingFriendshipList[indexPath.row]
+//        }
+//
+//        self.present(profileVC, animated: true, completion: nil)
+    }
 
 }
